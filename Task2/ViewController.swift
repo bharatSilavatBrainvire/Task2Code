e17f379c877866401f3bf412e1ccb77731a9ae23
bharatSilavatBrainvire <bharat.shilavat@brainvire.com>
2025-05-16 12:26:53 +0530

    Initial Commit

---
+++ Task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task2
+//
+//  Created by Bharat Shilavat on 16/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
